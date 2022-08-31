3c02a081ac9eb6a3813b262b9e97cc5c5bb09eba
Kyle Stubblefield <kyle.g.stubblefield@gmail.com>
2022-08-30 20:07:16 -0700

    fixing the version requirments'

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "ModelAuthenticationLimitMiddleware",
+    platforms: [
+       .macOS(.v12),
+    ],
     products: [
         .library(
             name: "ModelAuthenticationLimitMiddleware",
